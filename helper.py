c059fa9e556ff78248afa859744c9b8a9487a4a0
Tyler <tylerdback.etc@proton.me>
2023-02-05 23:29:35 -0500

    Added small function to view current contents of database to verify functions work as planned

--- helper.py
+++ helper.py
@@ -3,8 +3,24 @@
 
 # Import necessary libs...
 from datetime import datetime, date
+import sqlite3
 
 
 def get_current_date():
     """ Returns [current day, year] """
     return [datetime.now().timetuple().tm_yday, date.today().year]
+
+
+def testing_entries(db_file):
+    """ Used to verify queries are addedd successfully.
+        For testing purposes. Prints out all title in table """
+    conn = sqlite3.connect(db_file)
+    cur = conn.cursor()
+    
+    cur.execute("SELECT title,read FROM rmotdEntries")
+    rows = [row for row in cur.fetchall()]
+    for _ in rows:
+        print(_)
+
+    conn.commit()
+    conn.close()
